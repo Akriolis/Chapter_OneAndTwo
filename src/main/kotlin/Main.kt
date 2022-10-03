a9ab345be1a86ff82a7b1e410ff3b6ef87289d3a
Akriolis <Akriolis@gmail.com>
2022-10-03 14:16:12 +0400

    Added few lines of a practice code #3

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,12 @@
+import java.io.BufferedReader
+import java.io.StringReader
 import java.lang.Exception
+import java.lang.IllegalArgumentException
+import java.lang.NumberFormatException
+import java.lang.reflect.Executable
+import java.nio.Buffer
+import java.util.TreeMap
+
 
 fun main(){
     val persons = listOf<Person>(
@@ -115,6 +123,62 @@ fun main(){
 
     println(mix(Color.BLUE,Color.YELLOW))
 
+    println(mixOptimized(Color.VIOLET, Color.BLUE))
+
+    println(eval(Sum(Sum(Num(1), Num(2)), Num(4))))
+
+    println(eval3(Sum(Num(1), Num(2))))
+
+    println(evalWithLogging(Sum(Sum(Num(1),Num(2)), Num(3))))
+
+    for (i in 1..100){
+        println(fizzBuzz(i))
+    }
+
+    for (i in 100 downTo 1 step 2){
+        println(fizzBuzz(i))
+    }
+
+    val binaryReps = TreeMap<Char, String>()
+
+    for (c in 'A'..'F'){
+        val binary = Integer.toBinaryString(c.code)
+        binaryReps[c] = binary
+    }
+
+    for ((letter, binary) in binaryReps){
+        println("$letter = $binary")
+    }
+
+    val list = arrayListOf("10", "11", "1001")
+    for ((index, element) in list.withIndex()){
+        println("$index: $element")
+    }
+
+    println(isLetter('q'))
+    println(isNotDigit('x'))
+
+    println(recognize('A'))
+
+    println("Kotlin" in "Java".."Scala")
+    println("Kotlin" in setOf("Java", "Scala"))
+
+    /*val number = readLine()?.toInt()
+    val percentage =
+        if (number in 0..100)
+        "bingo!"
+        else
+            throw IllegalArgumentException(
+                "A percentage value must be between 0 and 100: $number"
+            )
+    println(percentage)*/
+
+    val reader = BufferedReader(StringReader("239"))
+    println(readNumber(reader))
+
+    val reader2 = BufferedReader(StringReader("Not a number"))
+    readNumber2(reader2)
+
 }
 /**
  * Functions and variables
@@ -146,6 +210,14 @@ fun max3 (a: Int, b: Int) = if (a > b) a else b
  * It corresponds to a regular (non-final) Java variable
  */
 
+/**
+ * Enum class
+ */
+
+/**
+ * when expression
+ */
+
 fun getMnemonic(color: Color) =
     when (color) {
         Color.RED -> "Richard"
@@ -170,3 +242,147 @@ fun mix (c1: Color, c2: Color) =
         setOf(Color.BLUE, Color.VIOLET) -> Color.INDIGO
         else -> throw Exception ("Dirty color")
     }
+
+fun mixOptimized (c1: Color, c2: Color) =
+    when{
+        (c1 == Color.RED && c2 == Color.YELLOW) ||
+                (c1 == Color.YELLOW && c2 == Color.RED) ->
+            Color.ORANGE
+
+        (c1 == Color.YELLOW && c2 == Color.BLUE) ||
+                (c1 == Color.BLUE && c2 == Color.YELLOW) ->
+            Color.GREEN
+
+        (c1 == Color.BLUE && c2 == Color.VIOLET) ||
+                (c1 == Color.VIOLET && c2 == Color.BLUE) ->
+            Color.INDIGO
+
+        else -> throw Exception("Dirty color")
+    }
+
+/**
+ * Smart cast
+ */
+
+interface Expr
+class Num(val value: Int): Expr
+class Sum (val left: Expr, val right: Expr): Expr
+
+fun eval (e: Expr): Int{
+    if (e is Num){
+        return e.value
+    }
+    if (e is Sum){
+        return eval (e.right) + eval (e.left)
+    }
+    throw IllegalArgumentException("Unknown expression")
+}
+
+// keyword is for check a certain type
+
+// keywords as for an explicit cast
+
+fun eval2 (e: Expr): Int =
+    if (e is Num){
+        e.value
+    } else if (e is Sum){
+        eval2 (e.right) + eval2(e.left)
+    } else{
+        throw IllegalArgumentException ("Unknown expression")
+    }
+
+fun eval3 (e: Expr): Int =
+    when (e) {
+        is Num -> {
+            e.value
+        }
+
+        is Sum -> {
+            eval3 (e.right) + eval3(e.left)
+        }
+
+        else -> {
+            throw IllegalArgumentException ("Unknown expression")
+        }
+    }
+
+fun evalWithLogging (e: Expr): Int =
+    when (e){
+        is Num -> {
+            println("num: ${e.value}")
+            e.value
+        }
+        is Sum -> {
+            val left = evalWithLogging(e.left)
+            val right = evalWithLogging(e.right)
+            println("sum: $left + $right")
+            left + right
+        }
+        else -> throw IllegalArgumentException("Unknown expression")
+    }
+
+// the last expression in a block is the result
+
+/**
+ * For, while and do-while loops
+ */
+
+// ranges - .. operator
+// val oneToTen = 1..10
+
+// ranges are closed or inclusive (meaning the second value is always a part of the range)
+
+fun fizzBuzz (i: Int) = when{
+    i % 15 == 0 -> "FizzBuzz"
+    i % 3 == 0 -> "Fizz"
+    i % 5 == 0 -> "Buzz"
+    else -> "$i "
+}
+
+fun isLetter(c: Char) = c in 'a'..'z' || c in 'A'..'Z'
+fun isNotDigit (c: Char) = c !in '0'..'9'
+
+fun recognize (c: Char) = when (c){
+    in '0'..'9' -> "It's a digit!"
+    in 'a'..'z', in 'A'..'Z' -> "It's a letter"
+    else -> "I don't know"
+}
+
+/**
+ * Exceptions in Kotlin
+ */
+
+fun readNumber(reader: BufferedReader): Int?{
+    try{
+        val line = reader.readLine()
+        return Integer.parseInt(line)
+    }
+    catch (e: NumberFormatException){
+        return null
+    }
+    finally {
+        reader.close()
+    }
+}
+
+// Kotlin doesn't differentiate between checked and unchecked exceptions
+
+// try as an expression
+
+fun readNumber2 (reader: BufferedReader){
+    val number = try{
+        Integer.parseInt(reader.readLine())
+    } catch (e: NumberFormatException){
+        null
+    }
+
+    println(number)
+}
+
+
+
+
+
+
+
+
